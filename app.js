eb8e1b48b88374263ac09bb74c4da5c1e805f434
Minko Minkov <minkom@outlook.com>
2017-10-08 21:56:24 -0700

    Fix error in app.js. Update package.json

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ var mobileApp = azureMobileApps({
     homePage: true,
     // Explicitly enable swagger support. UI support is enabled by
     // installing the swagger-ui npm module.
-    swagger: true,
+    swagger: true//,
     // App will use MS_SqliteFilename or MS_TableConnectionString to choose the SQLite or SQL data provider
-    data: {
-        provider: mssql,
-        dynamicSchema: true
-    }
+    //data: {
+    //    provider: "mssql",
+    //    dynamicSchema: true
+    //}
 });
 
 // Import the files from the tables directory to configure the /tables endpoint
